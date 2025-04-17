5c9fd5bb51d63b649adce040b15546e9b6bbf927
Fken19 <tb21l095@gmail.com>
2025-04-17 17:29:26 +0900

    Use FIRESTORE_KEY_JSON from environment

--- firestore_client.py
+++ firestore_client.py
@@ -1,4 +1,12 @@
+import os
+import json
 from google.cloud import firestore
+from google.oauth2 import service_account
 
-# Firestore のクライアントを初期化（環境変数 GOOGLE_APPLICATION_CREDENTIALS にサービスアカウントJSONのパスを設定しておくこと）
-db = firestore.Client()
+# 環境変数からFirestore認証情報（JSON文字列）を取得し、辞書に変換
+key_data = os.getenv("FIRESTORE_KEY_JSON")
+info = json.loads(key_data)
+
+# 認証情報を用いてFirestoreクライアントを初期化
+credentials = service_account.Credentials.from_service_account_info(info)
+db = firestore.Client(credentials=credentials, project=info["project_id"])
