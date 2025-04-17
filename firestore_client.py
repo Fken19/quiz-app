977e36dd453a5753ff6dca1d6063152b91b13ba9
Fken19 <tb21l095@gmail.com>
2025-04-17 17:40:55 +0900

    Use FIRESTORE_KEY_JSON_BASE64 instead of JSON

--- firestore_client.py
+++ firestore_client.py
@@ -1,10 +1,12 @@
 import os
 import json
+import base64
 from google.cloud import firestore
 from google.oauth2 import service_account
 
-# 環境変数からFirestore認証情報（JSON文字列）を取得し、辞書に変換
-key_data = os.getenv("FIRESTORE_KEY_JSON")
+# 環境変数からBase64エンコードされたFirestore認証情報を取得し、デコード
+key_base64 = os.getenv("FIRESTORE_KEY_JSON_BASE64")
+key_data = base64.b64decode(key_base64).decode("utf-8")
 info = json.loads(key_data)
 
 # 認証情報を用いてFirestoreクライアントを初期化
